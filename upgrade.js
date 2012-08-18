31be2dcd623f040cf71759373b2513d09358c1f9
Dan <cobbdb@gmail.com>
2012-08-18 01:29:05 -0700

    v0.1

--- upgrade.js
+++ upgrade.js
@@ -54,13 +54,13 @@ exports.writeHead = function (req, socket) {
 
 /**
  * @description Removes mask from incoming frame.
- * @param {Buffer} Buffer object from data event of WebSocket.
+ * @param {Buffer} buffer Buffer object from data event of WebSocket.
  * @returns {String} Unmasked data.
  * @see http://stackoverflow.com/questions/8125507/how-can-i-send-and-receive-websocket-messages-on-the-server-side
  * @since v0.1
  */
-exports.getData = function (data) {
-    var datalength = data[1] & 127;
+exports.getData = function (buffer) {
+    var datalength = buffer[1] & 127;
     var indexFirstMask = 2;
     if(datalength == 126) {
         indexFirstMask = 4;
@@ -68,12 +68,12 @@ exports.getData = function (data) {
         indexFirstMask = 10;
     }
     
-    var masks = data.slice(indexFirstMask, indexFirstMask + 4);
+    var masks = buffer.slice(indexFirstMask, indexFirstMask + 4);
     var i = indexFirstMask + 4;
     var j = 0;
     var output = "";
-    while(i < data.length) {
-        var charCode = data[i] ^ masks[j % 4];
+    while(i < buffer.length) {
+        var charCode = buffer[i] ^ masks[j % 4];
         output += String.fromCharCode(charCode);
         i += 1;
         j += 1;
